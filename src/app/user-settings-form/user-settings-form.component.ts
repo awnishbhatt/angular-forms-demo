31024457f977eeaaa26b2e78cb77dca66c6321b3
a b <forpluralsight1900@gmail.com>
2021-06-24 18:31:17 -0500

    - https://getbootstrap.com/docs/5.0/getting-started/introduction/ 	- https://getbootstrap.com/docs/5.0/forms/overview/
    - Add bootstrap to styles
                "styles": [
                  "node_modules/bootstrap/dist/css/bootstrap.css",
                  "src/styles.css"
                ],
    	- OR, add to the global "styles.css"
    		- @import "~bootstrap/dist/css/bootstrap.min.css";
    
    - ngForm
    	- {{ form.value | json }} //shows the value of all model's annotated in the form
    	- ngModel (1-way binding}
    		- uses the name attribute of the html element
    	- 2-way binding with example: [(ngModel)]="userSettings.name"
    		- TIP: create a copy of the values (for nested data structure, use lodash) - https://www.geeksforgeeks.org/lodash-_-clonedeep-method/
    		- shallow copy:   userSettings : UserSettings = {...this.originalUserSettings}; // this is called the "spread" syntax

---
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import {UserSettings} from "../data/user-settings";
+
+@Component({
+  selector: 'app-user-settings-form',
+  templateUrl: './user-settings-form.component.html',
+  styleUrls: ['./user-settings-form.component.css']
+})
+export class UserSettingsFormComponent implements OnInit {
+
+  originalUserSettings: UserSettings={
+    name : "thing",
+    emailOffers: true,
+    interfaceStyle: "dark",
+    subscriptionType:"2",
+    notes: "some notes"
+  }
+
+  userSettings : UserSettings = {...this.originalUserSettings};
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
